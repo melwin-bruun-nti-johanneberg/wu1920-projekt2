fc5504f69b1acea6ecf7608bc0eac5f49f08de98
melwin-bruun-nti-johanneberg <60173152+melwin-bruun-nti-johanneberg@users.noreply.github.com>
2020-03-18 09:49:59 +0100

    Update java.js

---
+++ js/java.js
@@ -0,0 +1,23 @@
+let togglenavstatus = false;
+
+let togglenav = function( ){
+    let getToggle = document.querySelector(".toggle-nav");
+    let getToggleul = document.querySelector(".ul-toggle")
+
+if (togglenavstatus === false){
+getToggleul.style.visibility = "visible"
+
+
+togglenavstatus = true ;
+}
+
+
+
+else if  (togglenavstatus === true){
+    getToggleul.style.visibility = "visible"
+    
+    
+    togglenavstatus = false;
+}
+    
+}
